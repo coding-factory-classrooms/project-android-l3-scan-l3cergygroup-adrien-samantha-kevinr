e828b22e817a4417390b968bf5c750fff3aa3623
Kev <rifakev@gmail.com>
2021-03-24 11:22:56 +0100

    [KR] - MAJ du ViewModel

--- app/src/main/java/com/kevin/foodscan/ficheproduit/FicheProduitViewModel.kt
+++ app/src/main/java/com/kevin/foodscan/ficheproduit/FicheProduitViewModel.kt
@@ -1,4 +1,16 @@
 package com.kevin.foodscan.ficheproduit
 
-class FicheProduitViewModel {
+import androidx.lifecycle.ViewModel
+
+data class FicheProduit(
+    val id: Int,
+    val nomProduit: String,
+    val marqueProduit: String,
+    val categoryProduit: String,
+    val ingredientsProduit: String,
+    val ingredientsInconnusProduit: Int,
+    val imageProduit: String
+    )
+
+class FicheProduitViewModel : ViewModel() {
 }
