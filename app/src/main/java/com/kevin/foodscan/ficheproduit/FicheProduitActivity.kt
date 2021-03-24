41e3cc95d1fb63295a1d9fb61d74ace1bcdcb871
Kev <rifakev@gmail.com>
2021-03-24 11:10:32 +0100

    [KR] - Création de l'activity Fiche Produit

---
+++ app/src/main/java/com/kevin/foodscan/ficheproduit/FicheProduitActivity.kt
@@ -0,0 +1,12 @@
+package com.kevin.foodscan.ficheproduit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kevin.foodscan.R
+
+class FicheProduitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fiche_produit)
+    }
+}
